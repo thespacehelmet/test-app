096ea253f624d72cf48fe466fb200878e70f7f27
jason <x@x.com>
2022-02-01 23:43:25 -0500

    working v1

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-const Navbar = () =>
-    return {
+const Navbar = () => {
+    return (
         <h1>Navbar</h1>
-    };
+    );
 
 }
 
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,6 @@
 export { default as Loader } from './Loader';
-export { default as Loader } from './Navbar';
-export { default as Loader } from './Footer';
-export { default as Loader } from './Services';
-export { default as Loader } from './Transactions';
-export { default as Loader } from './Welcome';
+export { default as Navbar } from './Navbar';
+export { default as Footer } from './Footer';
+export { default as Services } from './Services';
+export { default as Transactions } from './Transactions';
+export { default as Welcome } from './Welcome';
