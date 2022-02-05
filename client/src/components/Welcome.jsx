551da51a29f621680ac2f5aaa1c9dca8e6cd462f
jason <x@x.com>
2022-02-04 22:41:42 -0500

    updated welcome.jsx

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,6 +1,18 @@
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from 'react-icons/si';
+import { BsInfoCircle } from 'react-icons/bs';
+
+import { Loader } from './';
+
 const Welcome = () => {
     return (
-        <h1>Welcome</h1>
+        /** This is the main container */
+        <div className="flex w-fell justify-center items-center">
+            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px" ></div>
+
+
+
+        </div>
     );
 
 }
